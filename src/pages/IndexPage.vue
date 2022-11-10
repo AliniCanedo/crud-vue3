173061ae33a0fa7851003e42e63f24340feb0a16
Alini Canedo <alini.canedo@gmail.com>
2022-11-10 19:46:38 -0300

    listing the posts and configuring the env

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,45 @@
 <template>
   <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { api } from 'boot/axios'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+    const columns = [
+      { name: 'id', field: 'id', label: 'id', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'Title', sortable: true, align: 'left' },
+      { name: 'author', field: 'author', label: 'Author', sortable: true, align: 'left' }
+    ]
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    const getPosts = async () => {
+      try {
+        const { data } = await api.get('posts')
+        posts.value = data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    return {
+      posts,
+      columns
+    }
+  }
 })
 </script>
